d652cde7f141d92e047d657d22495177e15a8aea
syedp <syedp@LAPTOP-EH540P3B>
2026-06-03 15:18:47 +0530

    Added Batch Processing System project using Java, JDBC, and PostgreSQL

---
+++ JDBC_learning/src/main/java/students/insertdata.java
@@ -0,0 +1,42 @@
+package students;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class insertdata {
+	public static void main(String[] args) {
+		try {
+			Class.forName("org.postgresql.Driver");
+			String url = "jdbc:postgresql://localhost:5432/jdbcdb";
+			String user = "postgres";
+			String password = "root123";
+			
+			Connection connection = DriverManager.getConnection(url,user,password);
+			
+			String sql = "insert into students values(?,?,?,?,?)";
+			PreparedStatement ps = connection.prepareStatement(sql);
+			ps.setInt(1,100);
+			ps.setString(2,"Syed");
+			ps.setDouble(3,55.8);
+			ps.setBoolean(4,false);
+			ps.setDate(5,Date.valueOf("2001-05-01"));
+			ps.addBatch();
+			
+			ps.setInt(1,101);
+			ps.setString(2,"Sharmili");
+			ps.setDouble(3,54.8);
+			ps.setBoolean(4,true);
+			ps.setDate(5,Date.valueOf("2001-04-01"));
+			ps.addBatch();
+			ps.executeBatch();
+			System.out.println("Data executed successfully");
+			connection.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
